de41c5a3626d4e9c75d79acf9969f4fe53427526
unknown <ianmmah@gmail.com>
2021-06-11 13:12:12 -0700

    Create card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Button from './components/Button'
+import Card from './components/Card'
 import SongList from './components/SongList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button onClick={() => console.log('Clicked Login')}>Login</Button>
+        <Card>
+          <h4>Login</h4>
+          <Button onClick={() => console.log('Clicked Login')}>Login</Button>
+        </Card>
+        
         <SongList />
       </header>
     </div>
