0a1257f714bde5461338fb524154d2fd61678034
Adin Kwok <adin@adin.dev>
2021-06-20 14:57:45 -0700

    Setup redux with dark mode state management

---
+++ src/state/management/userSettings.js
@@ -0,0 +1,30 @@
+// Initial state
+const INITIAL_STATE = {
+  isDarkModeEnabled: false,
+};
+
+// Types
+export const TOGGLE_DARK_MODE = 'TOGGLE_DARK_MODE';
+
+// Actions
+export const toggleDarkMode = (isDarkModeEnabled) => {
+  return {
+    type: TOGGLE_DARK_MODE,
+    isDarkModeEnabled: isDarkModeEnabled,
+  };
+};
+
+// Reducer
+function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case TOGGLE_DARK_MODE:
+      return {
+        ...state,
+        isDarkModeEnabled: action.isDarkModeEnabled,
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
---
+++ src/state/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { userSettings } from './management/userSettings';
+
+const rootReducer = combineReducers({
+  userSettings: userSettings,
+});
+
+export default rootReducer;
