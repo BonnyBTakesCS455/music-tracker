8ddc0eb16c195122ad862fa0431d29f2da8c3194
Adin Kwok <adin@adin.dev>
2021-07-17 01:53:31 -0700

    Add 'add friend by Spotify ID' search to side bar

--- src/components/FriendsSidebar.jsx
+++ src/components/FriendsSidebar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-import SlidingPane from "react-sliding-pane";
-import "../css/SldiingPane.css"
-import FriendsIcon from "../icons/Friends_Filled_WHITE.png"
-import Friend from "./Friend";
+import SlidingPane from 'react-sliding-pane';
+import '../css/SldiingPane.css';
+import FriendsIcon from '../icons/Friends_Filled_WHITE.png';
+import Friend from './Friend';
+import FriendsSearchBar from './FriendsSearchBar';
 
 const StickySidebar = styled.div`
-    position: -webkit-sticky;
-    position: sticky;
-    top: 0;
-    background-color: #1db954;
-    border: 2px solid #1db954;
-    padding: 4px 4px;
-    width: 50px;
-    border-radius: 0px 25px 25px 0px;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  background-color: #1db954;
+  border: 2px solid #1db954;
+  padding: 4px 4px;
+  width: 50px;
+  border-radius: 0px 25px 25px 0px;
 `;
 
 const StyledImage = styled.img`
@@ -25,44 +26,42 @@ const StyledImage = styled.img`
 `;
 
 class FriendsSidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
 
-    constructor(props) {
-        super(props);
-        this.state = {show: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+  handleClick() {
+    this.setState((prevState) => {
+      return { show: !prevState.show };
+    });
+  }
 
-    handleClick() {
-        this.setState((prevState) => {
-            return {show: !prevState.show }
-        });
-    }
-
-    render() {
-        return (
-
-            <React.Fragment>
-                <StickySidebar onClick={this.handleClick}>
-                    <StyledImage
-                        src={FriendsIcon}/>
-                </ StickySidebar>
-                <SlidingPane
-                    className={"Pane"}
-                    overlayClassName={"Pane"}
-                    isOpen={this.state.show}
-                    onRequestClose={() => {
-                        this.setState({show:false});
-                    }}
-                    from={"left"}
-                    width={"400px"}
-                    title={"Currently Listening"}
-                >
-                <Friend />
-                </SlidingPane>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <StickySidebar onClick={this.handleClick}>
+          <StyledImage src={FriendsIcon} />
+        </StickySidebar>
+        <SlidingPane
+          className={'Pane'}
+          overlayClassName={'Pane'}
+          isOpen={this.state.show}
+          onRequestClose={() => {
+            this.setState({ show: false });
+          }}
+          from={'left'}
+          width={'400px'}
+          title={'Currently Listening'}
+        >
+          <Friend />
+          <FriendsSearchBar />
+        </SlidingPane>
+      </React.Fragment>
+    );
+  }
 }
 
 export default FriendsSidebar;
