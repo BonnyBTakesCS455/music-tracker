cae6f5b708163e37ce3f3b23d3cbfffe4f73ab6d
Samantha Huang <samantha.h@athinkingape.com>
2021-07-17 00:00:54 -0700

    Migrate settings page to user profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Settings from '../components/Settings';
 
 function Profile(props) {
   return (
     <React.Fragment>
       <header className='App-header'>
         <h1>The user profile page</h1>
+        <Settings />
       </header>
     </React.Fragment>
   );
