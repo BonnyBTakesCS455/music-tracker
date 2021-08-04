2e58342a990b6683fc642326bb0e3f7d60d4b545
Ian Mah <ianmmah@gmail.com>
2021-08-03 21:49:30 -0700

    Login page styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 import Card from "../components/Card";
 import Button from "../components/Button";
 import { login } from "../services";
+
+const LoginContainer = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url("https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2550&q=80"); 
+  background-size: cover;
+  background-position: center;
+`
+
+const ButtonContainer = styled.div`
+  margin-top: 30vh;
+  z-index: 100;
+`
+
+const Filter = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  position: absolute;
+  height: 100%;
+  mix-blend-mode: color;
+  background: black;
+  z-index: 10;
+`
+
+const Filter2 = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  position: absolute;
+  height: 100%;
+  mix-blend-mode: soft-light;
+  background: linear-gradient(#20d10f 60%, black);
+  z-index: 11;
+`
 
 function Login() {
   /**
@@ -17,14 +55,13 @@ function Login() {
   }
 
   return (
-    <>
-      <header className="App-container">
-        <Card>
-          <h4>Login</h4>
-          <Button onClick={goToSpotifyAuthLogin}>Login</Button>
-        </Card>
-      </header>
-    </>
+    <LoginContainer>
+      <ButtonContainer>
+        <Button onClick={goToSpotifyAuthLogin}>Login</Button>
+      </ButtonContainer>
+      <Filter />
+      <Filter2 />
+    </LoginContainer>
   );
 }
 
