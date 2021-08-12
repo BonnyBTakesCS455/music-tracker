07bc8dad1e786ed9260d74373398b4f9c3a30c96
unknown <ianmmah@gmail.com>
2021-08-12 09:35:57 -0700

    Prettier

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -14,8 +14,8 @@ function Graph({ artistData }) {
       <BarChart width={800} height={1000} data={artistData} layout="vertical">
         <defs>
           <linearGradient id="barGrad" x1="0" y1="0" x2="1" y2="0">
-            <stop offset="0%" stopColor="#1db954" stopOpacity={0.8}/>
-            <stop offset="100%" stopColor="#1db954" stopOpacity={0.4}/>
+            <stop offset="0%" stopColor="#1db954" stopOpacity={0.8} />
+            <stop offset="100%" stopColor="#1db954" stopOpacity={0.4} />
           </linearGradient>
         </defs>
         <CartesianGrid stroke="#444" />
@@ -32,10 +32,10 @@ function Graph({ artistData }) {
           tick={{ fontSize: 14, fill: "grey" }}
         />
         <Bar
-        dataKey="plays"
-        stroke="#1db954"
-        fill="url(#barGrad)"
-        layout="vertical"
+          dataKey="plays"
+          stroke="#1db954"
+          fill="url(#barGrad)"
+          layout="vertical"
         />
       </BarChart>
     </Container>
