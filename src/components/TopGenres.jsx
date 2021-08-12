76cc55e4ed0b15577cc8a9d51330cc569472dd92
unknown <vandyliu1999@gmail.com>
2021-08-12 00:22:50 -0700

    format'

--- src/components/TopGenres.jsx
+++ src/components/TopGenres.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Radar,
-  RadarChart,
-  PolarGrid,
-  PolarAngleAxis,
-} from "recharts";
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis } from "recharts";
 
 function TopGenres({ genres }) {
   if (!genres) return <></>;
