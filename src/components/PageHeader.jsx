e91d3a4a06e22ddccd6aa67047321933afe15bd1
Adin Kwok <adin@adin.dev>
2021-08-12 11:03:42 -0700

    Simple copy ID icon in header

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import profile_icon from "../icons/Dot_Hollow.png";
+import copy_icon from "../icons/copy.png";
 
 const Img = styled.img`
   position: relative;
@@ -22,11 +23,37 @@ const PageHeaderContainer = styled.div`
   width: 100%;
 `;
 
+const Icon = styled.img`
+  position: relative;
+  max-width: 24px;
+  max-height: 24px;
+`;
+
+const CopyText = styled.div`
+  font-size: 10px;
+`;
+
+const Copy = styled.div`
+  cursor: pointer;
+`;
+
 function PageHeader(props) {
+  const handleClick = async () => {
+    try {
+      await navigator.clipboard.writeText(props.spotifyId);
+    } catch (err) {
+      // Oh well
+    }
+  };
+
   return (
     <PageHeaderContainer>
       <Img src={profile_icon} width={72} height={72} />
       <Title>{props.title}</Title>
+      <Copy onClick={handleClick}>
+        <Icon alt="copy" src={copy_icon} />
+        <CopyText>Copy ID</CopyText>
+      </Copy>
     </PageHeaderContainer>
   );
 }
